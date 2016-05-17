61be2bbb3582421077297e4149fcb0970da6b71c
Adrian Limon <me@adrianlimon.com>
2016-05-16 22:15:45 -0700

    added calculator project

---
+++ tuts/Calculator/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void ShouldAdd()
+        {
+            var sut = new Calculator();
+
+            var result = sut.Add(1, 2);
+
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ tuts/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+
+}
