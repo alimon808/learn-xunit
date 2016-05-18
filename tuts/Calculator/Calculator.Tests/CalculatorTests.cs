34770bb313ef5dbacbc10fb64c3bba30c19b9985
Adrian Limon <me@adrianlimon.com>
2016-05-18 09:50:14 -0700

    test categories using traits

--- tuts/Calculator/Calculator.Tests/CalculatorTests.cs
+++ tuts/Calculator/Calculator.Tests/CalculatorTests.cs
@@ -1,7 +1,10 @@
-﻿using Xunit;
+﻿using System;
+using System.Threading;
+using Xunit;
 
 namespace Calculator.Tests
 {
+    [Trait("Category", "Error Checking")]
     public class CalculatorTests
     {
         [Fact]
@@ -12,6 +15,36 @@ namespace Calculator.Tests
             var result = sut.Add(1, 2);
 
             Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void ShouldErrorWhenDivideByZero()
+        {
+            var sut = new Calculator();
+
+            Assert.Throws<DivideByZeroException>(() => sut.Divide(10, 0));
+        }
+
+        [Fact]
+        public void ShouldErrorWhenNumberTooBig()
+        {
+            var sut = new Calculator();
+
+            ArgumentOutOfRangeException thrownExeption = Assert.Throws<ArgumentOutOfRangeException>(() => sut.Divide(201, 1));
+
+            Assert.Equal("value", thrownExeption.ParamName);
+        }
+
+        [Fact]
+        [Trait("Category", "Slow Running")]
+        public void ShouldErrorWhenNumberTooBig_SLOW()
+        {
+            var sut = new Calculator();
+
+            // simulate long running test
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            ArgumentOutOfRangeException thrownException = Assert.Throws<ArgumentOutOfRangeException>(() => sut.Divide(201, 1));
         }
     }
 }
