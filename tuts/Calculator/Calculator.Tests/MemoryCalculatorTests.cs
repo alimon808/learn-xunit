99dfc5b17e0a25286ae44fa8ef0ce8d368cfb7e5
Adrian Limon <me@adrianlimon.com>
2016-05-18 10:11:10 -0700

    created MemoryCalculator with tests

---
+++ tuts/Calculator/Calculator.Tests/MemoryCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Calculator.Tests
+{
+    public class MemoryCalculatorTests
+    {
+        private readonly ITestOutputHelper _testOutput;
+
+        public MemoryCalculatorTests(ITestOutputHelper helper)
+        {
+            _testOutput = helper;
+        }
+
+        [Fact]
+        public void ShouldAdd()
+        {
+            _testOutput.WriteLine("Executing ShouldAdd");
+
+            using (var sut = new MemoryCalculator())
+            {
+                sut.Add(10);
+                sut.Add(5);
+
+                Assert.Equal(15, sut.CurrentValue);
+            }
+        }
+
+        [Fact]
+        public void ShouldSubtract()
+        {
+            _testOutput.WriteLine("Executing ShouldSubtract");
+
+            using (var sut = new MemoryCalculator())
+            {
+                sut.Subtract(5);
+
+                Assert.Equal(-5, sut.CurrentValue);
+            }
+        }
+
+        [Fact]
+        public void ShouldDivide()
+        {
+            _testOutput.WriteLine("Executing ShouldDivide");
+
+            using (var sut = new MemoryCalculator())
+            {
+                sut.Add(10);
+                sut.Divide(2);
+
+                Assert.Equal(5, sut.CurrentValue);
+            }
+        }
+    }
+}
---
+++ tuts/Calculator/Calculator/MemoryCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MemoryCalculator : IDisposable
+    {
+        public int CurrentValue { get; private set; }
+
+        public void Add(int number)
+        {
+            CurrentValue += number;
+        }
+
+        public void Subtract(int number)
+        {
+            CurrentValue -= number;
+        }
+
+        public void Divide(int number)
+        {
+            CurrentValue = CurrentValue / number;
+        }
+
+        public void Dispose()
+        {
+            // clean up resources
+        }
+    }
+}
