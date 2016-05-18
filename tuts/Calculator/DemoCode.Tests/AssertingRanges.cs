24f637ea588a077eeaa9143cc7042942dbbe09b3
Adrian Limon <me@adrianlimon.com>
2016-05-18 09:58:16 -0700

    write diagnostic messages to standard ouput

--- tuts/Calculator/DemoCode.Tests/AssertingRanges.cs
+++ tuts/Calculator/DemoCode.Tests/AssertingRanges.cs
@@ -1,15 +1,28 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace DemoCode.Tests
 {
     public class AssertingRanges
     {
+        private readonly ITestOutputHelper _testOutput;
+
+        public AssertingRanges(ITestOutputHelper helper)
+        {
+            _testOutput = helper;
+        }
+
         [Fact]
         public void ShouldIncreaseHealthAfterSleeping()
         {
+            _testOutput.WriteLine("Creating PlayerCharacter");
             var sut = new PlayerCharacter { Health = 100 };
 
+            _testOutput.WriteLine("PlayerCharacter going to sleep");
             sut.Sleep();
+            _testOutput.WriteLine("PlayerCharacter awoken");
+
 
             Assert.InRange(sut.Health, 101, 200);
         }
