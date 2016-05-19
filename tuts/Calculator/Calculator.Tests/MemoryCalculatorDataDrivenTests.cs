2c7861eaeea669d98a3f84506c351f6725d7ad29
Adrian Limon <me@adrianlimon.com>
2016-05-19 11:12:11 -0700

    data driven tests initial state

---
+++ tuts/Calculator/Calculator.Tests/MemoryCalculatorDataDrivenTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Calculator.Tests
+{
+    public class MemoryCalculatorDataDrivenTests
+    {
+        [Fact]
+        public void ShouldSubtractTwoPositiveNumbers()
+        {
+            var sut = new MemoryCalculator();
+
+            sut.Subtract(5);
+            sut.Subtract(10);
+
+            Assert.Equal(-15, sut.CurrentValue);
+        }
+
+        [Fact]
+        public void ShouldSubtractTwoNegativeNumbers()
+        {
+            var sut = new MemoryCalculator();
+
+            sut.Subtract(-5);
+            sut.Subtract(-10);
+
+            Assert.Equal(15, sut.CurrentValue);
+        }
+        
+        [Fact]
+        public void ShouldSubtractZero()
+        {
+            var sut = new MemoryCalculator();
+
+            sut.Subtract(10);
+            sut.Subtract(0);
+
+            Assert.Equal(-10, sut.CurrentValue);
+        }
+    }
+}
