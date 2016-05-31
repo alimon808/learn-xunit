370ea798542285e96dee5d0ec8baa0c5f21db88f
Adrian Limon <me@adrianlimon.com>
2016-05-31 11:07:50 -0700

    added simple default homecontroller

---
+++ tuts/TDD/TDD.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TDD.Controllers;
+using Xunit;
+
+namespace TDD.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void ShouldReturnDefaultView()
+        {
+            var sut = new HomeController();
+
+            ViewResult result = sut.Index() as ViewResult;
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void ShouldReturnContactView()
+        {
+            var sut = new HomeController();
+
+            ViewResult result = sut.Contact() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Your contact page.", result.ViewBag.Message);
+        }
+
+        [Fact]
+        public void ShouldReturnAboutView()
+        {
+            var sut = new HomeController();
+
+            ViewResult result = sut.About() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Your about page.", result.ViewBag.Message);
+        }
+
+    }
+}
---
+++ tuts/TDD/TDD/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TDD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            ViewBag.Message = "Your about page.";
+
+            return View();
+        }
+    }
+
+}
